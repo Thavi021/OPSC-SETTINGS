44a0b2a46b9aec75b06c32d12d426328e602972b
Thaveshan Naidoo <96347605+Thavi021@users.noreply.github.com>
2022-10-11 16:39:35 +0200

    Update SettingsActivity.java

--- SettingsActivity.java
+++ SettingsActivity.java
@@ -1,14 +1,1 @@
-package com.example.finalnavigationappv11;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class SettingsActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-    }
-}
